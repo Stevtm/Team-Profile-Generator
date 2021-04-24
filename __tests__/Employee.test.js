77e4e3c7d1aa8ce2b651d414b8f3daec98cb2ddc
Stevtm <steventmaddison@outlook.com>
2021-04-24 15:53:57 -0400

    pass Employee class test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,13 @@
 const Employee = require("../lib/Employee");
 
 test("creates an employee object", () => {
-	const employee = new Employee("John");
+	const employee = new Employee("John", 2, "john@smith.com");
 
 	expect(employee.name).toBe("John");
 	expect(employee.id).toEqual(expect.any(Number));
+	expect(employee.id).toEqual(2);
 	expect(employee.email).toEqual(expect.any(String));
+	expect(employee.email).toEqual("john@smith.com");
 });
 
 // name, id, email, getName(), getId(), getEmail(), getRole()
