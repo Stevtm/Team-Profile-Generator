57de2508e7d0bb34395def67a703ac6346911608
Stevtm <steventmaddison@outlook.com>
2021-04-24 17:04:22 -0400

    fail Manager method tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,36 @@
+const Manager = require("../lib/Manager");
+
+// extends Employee, officeNumber, getRole()
+
+test("creates a manager object", () => {
+	const manager = new Manager("John", 2, "john@smith.com", 101);
+
+	expect(manager.name).toBe("John");
+	expect(manager.id).toBe(2);
+	expect(manager.email).toBe("john@smith.com");
+	expect(manager.officeNumber).toBe(101);
+});
+
+test("tests the getName() method", () => {
+	const manager = new Manager("John", 2, "john@smith.com", 101);
+
+	expect(manager.getName()).toBe("John");
+});
+
+test("tests the getId() method", () => {
+	const manager = new Manager("John", 2, "john@smith.com", 101);
+
+	expect(manager.getId()).toBe(2);
+});
+
+test("tests the getEmail() method", () => {
+	const manager = new Manager("John", 2, "john@smith.com", 101);
+
+	expect(manager.getEmail()).toBe("john@smith.com");
+});
+
+test("tests the getRole() method", () => {
+	const manager = new Manager("John", 2, "john@smith.com", 101);
+
+	expect(manager.getRole()).toBe("Manager");
+});
