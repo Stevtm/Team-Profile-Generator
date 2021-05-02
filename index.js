eaa5cb47a6d1df33e05750969c8b4c328c5cef06
Stevtm <steventmaddison@outlook.com>
2021-05-01 20:16:39 -0400

    configure prompt flow and output object

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const inquirer = require("inquirer");
 
+// create an object to hold all personnel information
+const personnel = {
+	manager: [],
+	engineers: [],
+	interns: [],
+};
+
 // arrays of questions for prompts
 // manager questions
 const managerQuestions = [
@@ -162,8 +169,69 @@ const internQuestions = [
 	},
 ];
 
-const promptUser = (questions) => {
-	return inquirer.prompt(questions);
+const promptAddEmployeeQuestion = () => {
+	return inquirer
+		.prompt([
+			{
+				type: "list",
+				name: "addEmployee",
+				message: "Would you like to add an Engineer or an Intern?",
+				choices: [
+					"Add an Engineer",
+					"Add an Intern",
+					"Don't add any Employees",
+				],
+				default: "Add an Engineer",
+			},
+		])
+		.then((addEmployeeData) => {
+			if (addEmployeeData.addEmployee === "Add an Engineer") {
+				return promptEngineer(personnel);
+			} else if (addEmployeeData.addEmployee === "Add an Intern") {
+				return promptIntern(personnel);
+			} else {
+				return personnel;
+			}
+		});
 };
 
-promptUser(engineerQuestions).then((res) => console.log(res));
+const promptManager = () => {
+	// prompt the user to answer the manager questions
+	return inquirer.prompt(managerQuestions).then((managerData) => {
+		personnel.manager.push(managerData);
+	});
+};
+
+const promptEngineer = () => {
+	console.log(`
+	=================
+	Add a New Engineer
+	=================`);
+
+	return inquirer
+		.prompt(engineerQuestions)
+		.then((engineerData) => {
+			personnel.engineers.push(engineerData);
+		})
+		.then(promptAddEmployeeQuestion);
+};
+
+const promptIntern = () => {
+	console.log(`
+	=================
+	Add a New Intern
+	=================`);
+
+	return inquirer
+		.prompt(internQuestions)
+		.then((internData) => {
+			personnel.interns.push(internData);
+		})
+		.then(promptAddEmployeeQuestion);
+};
+
+promptManager()
+	.then(promptAddEmployeeQuestion)
+	.then((personnel) => {
+		console.log(personnel);
+	});
